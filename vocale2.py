c7c94b288ef25da6d457200385c41b8691c001a2
JeanBonnard <j.bonnard42@gmail.com>
2016-12-09 18:20:19 +0100

    modif

--- vocale2.py
+++ vocale2.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
-
+import subprocess
 import pygame
+
+
+			#pico2wave -l fr-FR -w $.wav "$";
+subprocess.call(["pico2wave", "-l", "fr-FR", "-w", "$.wav", "\"$\";"])
+
 pygame.mixer.init()
-pygame.mixer.Sound("test.wav").play()
+pygame.mixer.Sound("bonjour.wav").play()
+
 pygame.mixer.Sound("$.wav").play()
 while pygame.mixer.get_busy():
     # lecture en cours
